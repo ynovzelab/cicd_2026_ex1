5f94b559dd887c144154fff6a59d9c10e9084bd0
Vincent Zerbib <contact@zelabstudio.com>
2026-02-13 11:40:01 +0100

    inti

---
+++ eslint.config.mts
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    ignores: ["dist/*", "node_modules/*"],
+  },
+  {
+    files: ["./src/**/*.{ts,mts,cts}"], 
+    plugins: { js }, extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+]);
---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import products from "./data/products.json";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+interface Product {
+  id: number;
+  name?: string;
+  price?: number;
+  description?: string;
+}
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/products/:id", (req, res) => {
+  const productId = parseInt(req.params.id, 10);
+  const product = products.find((p: Product) => p.id === productId);
+  res.json(product);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
